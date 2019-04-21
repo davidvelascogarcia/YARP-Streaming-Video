85e4daedb6499b5543dd830a2e9aea57aaf68012
davidvelascogarcia <davidvelascogarcia@gmail.com>
2019-04-21 14:07:59 +0200

    Format fixes

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # YARP: Streaming Video
-[![Build Status](https://travis-ci.org/davidvelascogarcia/YARP-Streaming-Video.svg?branch=develop)](https://travis-ci.org/davidvelascogarcia/YARP-Streaming-Video)
+[![Build Status](https://travis-ci.org/davidvelascogarcia/yarp-streaming-video.svg?branch=develop)](https://travis-ci.org/davidvelascogarcia/yarp-streaming-video)
 
 - [Introducción](#introducción)
+- [Listado de programas](#listado-de-programas)
 - [Enlaces de interés](#enlaces-de-interés)
 
 ## Introduccción
 
-YARP Streaming Video. 
-- Aplicación para el streaming de video en red, con emisor y receptor
-- Envío vía YARP, y con reconocimiento de personas vía OpenCV.
+`yarpStreamingVideo` es una implementación de software para el streaming de video en red, con emisor y receptor. Envío vía `YARP`, y con reconocimiento de personas vía `OpenCV` clasificador `haar`.
+
+## Listado de programas
+
+Se pueden encontar el conjunto de aplicaciones desarrolladas en [./programs](./programs).
 
 ## Enlaces de interés
 
-[Programa](./programs)
+* [YARP](http://www.yarp.it/)
---
+++ programs/linux/yarpStreamingVideo/sender/main.cpp
@@ -0,0 +1,61 @@
+//****************************************************
+//****************************************************
+//
+//              YARP: Streaming Video
+//
+//              Tipo: Emisor
+//
+//              Autor: David Velasco García
+//		       @davidvelascogarcia
+//
+//****************************************************
+//****************************************************
+
+// Librerias
+
+#include <iostream>
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <opencv2/opencv.hpp>
+#include <yarp/sig/Image.h>
+
+
+// Espacios de nombres
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::sig::draw;
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout<< "Bienvenido al emisor de imágen en streaming vía YARP"<<endl;
+    cout<<endl;
+    cout<<endl;
+    Network yarp;
+    cout<<"Iniciando red de YARP..."<<endl;
+    Port puerto_envio;  	
+    cout<<"Iniciando cliente..."<<endl;
+    cout<<"Abriendo puerto de envío con nombre /yarpStreamingVideo/img:o"<<endl;
+    puerto_envio.open("/emisor_video");
+//    Network::connect("/yarpStreamingVideoSender/img:o","/yarpStreamingVideoSender/img:i");
+    cout<<"Enviando fuentes de imágen.."<<endl;
+    VideoCapture cap;
+    Mat imagen;
+    if(!cap.open(0))
+	cout << "Lo siento, no puedo acceder a la webcam, revise la conexión" << endl;
+	while(true)
+	{	
+		cap >> imagen;
+		ImageOf<PixelBgr> B; 
+   		B.setExternal(imagen.data,imagen.size[1],imagen.size[0]);
+   		puerto_envio.write(B); 
+}
+    return 0;
+}
+
+
+
+
+
